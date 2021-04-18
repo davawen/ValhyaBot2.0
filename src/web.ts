72001ff7a0f708652bfb922ce9988bb69863439e
davawen <56738724+davawen@users.noreply.github.com>
2021-04-18 16:50:23 +0200

    fix port again

--- src/web.ts
+++ src/web.ts
@@ -4,7 +4,7 @@ import { streamers } from './main';
 import { TwitchStreamWebhook } from './api';
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 export const recieveWebhooks = () =>
 {
