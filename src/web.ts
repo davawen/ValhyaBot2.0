18650a070ee6471286621532ccc11c083f28e1cf
davawen <56738724+davawen@users.noreply.github.com>
2021-04-18 17:35:04 +0200

    Respond to twitch webhook

--- src/web.ts
+++ src/web.ts
@@ -1,28 +1,44 @@
 import * as express from 'express';
 
 import { streamers } from './main';
-import { TwitchStreamWebhook } from './api';
+import { request, TwitchStreamWebhook } from './api';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 export const recieveWebhooks = () =>
 {
 	app.get('/twitch',
 		(req, res) =>
 		{
-			console.log(req);
+			console.log(req.body);
 			
-			console.log(res);
+			if(req.body['hub.challenge'])
+			{
+				res.type('text/plain');
+				res.status(200).send(req.body['hub.challenge']);
+			}
+		}
+	)
+	
+	app.post('/twitch',
+		(req, res) =>
+		{
+			console.log(req.body);
 			
 			try
 			{
+				res.status(200).send('Sucess ! :)');
+				
 				if(req.body.length <= 0) return; //Stream offline
 				
 				let stream: TwitchStreamWebhook = req.body[0];
 				let streamer = streamers.get(stream.user_login);
 				
-				console.log(stream);
+				//console.log(stream);
 				
 				streamer.channels.forEach(
 					channel =>
